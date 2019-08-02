e451b6d2c09ed0334d963f176f4ca45d1d3af679
Clifford Tran <clifford@phoenixs-mbp.lan>
2019-08-01 23:31:57 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const searchUrl = 'https://api.github.com/users/'
+const searchUrl = 'https://developer.nps.gov/api/v1/places?'
+const apiKey = 'api_key=z2hPxOk745e1ir6aS8UO3ro3YncW3fVWbsfSUOzv'
 
-//https://api.github.com/users/clipqq/repos
+// https://developer.nps.gov/api/v1/places?stateCode=CA&stateCode=TX&api_key=z2hPxOk745e1ir6aS8UO3ro3YncW3fVWbsfSUOzv
+
+// https://developer.nps.gov/api/v1/parks
+// ?parkCode=acad&api_key=z2hPxOk745e1ir6aS8UO3ro3YncW3fVWbsfSUOzv
 
 function apiRequest(userInput) {
     const urlString = createApiString(userInput)
@@ -8,8 +12,15 @@ function apiRequest(userInput) {
 }
 
 function createApiString(userInput) {
-
-    const urlString = `${searchUrl}${userInput}/repos`
+    // search ?places=statecode
+    console.log(`working with ${userInput}`)
+    const stateArray = userInput.split(',')
+    const stateSnippet = []
+    for (let i=0;i<stateArray.length;i++) {
+        stateSnippet.push(`stateCode=${stateArray[i]}&`)
+    }
+    const joinedState = stateSnippet.join('')
+    const urlString = `${searchUrl}${joinedState}${apiKey}`
     console.log(`making URL  ${urlString}`)
     return urlString
 }
@@ -37,10 +48,11 @@ function makeFetchRequest(urlString) {
 
 function renderHTML(jsonResult) {
     console.log(jsonResult)
+    $('.results').empty()
     for (let i = 0; i < jsonResult.length; i++) {
         const repoName = jsonResult[i].name
         const repoUrl = jsonResult[i].url
-    
+
         $('.results').append(`
         <div class='repo-name'>${repoName}</div>
         <div class="repo-url">${repoUrl}</div>
